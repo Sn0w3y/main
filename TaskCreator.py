af62ed54d0dcb3821023d2a6509bbe54dde655b9
Sn0w3y <hallerjo97@gmail.com>
2023-01-23 09:31:30 +0100

    Update TaskCreator.py

--- TaskCreator.py
+++ TaskCreator.py
@@ -1,18 +1,11 @@
 class ReadTask:
-    def __init__(self, device, register, priority):
+    def __init__(self, device, register, priority, slave):
         self.device = device
         self.register = register
         self.priority = priority
+        self.slave = slave
 
     def execute(self):
         # code to read data from the device using pymodbus
-        result = self.device.read_holding_registers(self.register, 1)
+        result = self.device.read_holding_registers(self.register, 1, unit=self.slave)
         print(result.registers)
-
-    def get_execute_duration(self):
-        # expected duration of the read operation
-        return 200
-
-# Create a ReadTask
-task1 = ReadTask(client, 1, Priority.HIGH)
-task2 = ReadTask(client, 2, Priority.LOW)
